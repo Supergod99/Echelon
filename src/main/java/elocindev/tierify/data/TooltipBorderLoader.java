fd1199c05210888a70ad9f8c923d832aa7110a11
Supergod99 <rsupergod99@gmail.com>
2025-12-01 00:36:59 -0500

    Update TooltipBorderLoader.java
    fix overwriting modifier ID

--- src/main/java/elocindev/tierify/data/TooltipBorderLoader.java
+++ src/main/java/elocindev/tierify/data/TooltipBorderLoader.java
@@ -48,7 +48,7 @@ public class TooltipBorderLoader implements SimpleSynchronousResourceReloadListe
                         
                         // Decider predicate for Perfect items
                         decider.clear();
-                        decider.add("{Tier:\"tiered:perfect\"}");
+                        decider.add("{BorderTier:\"tiered:perfect\"}");
                     }
 
                     TierifyClient.BORDER_TEMPLATES.add(new BorderTemplate(data2.get("index").getAsInt(), data2.get("texture").getAsString(),
