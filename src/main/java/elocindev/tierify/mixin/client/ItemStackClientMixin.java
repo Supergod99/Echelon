9483b33ed4208dd0a02229e94064060a0039d2cd
Supergod99 <rsupergod99@gmail.com>
2025-12-03 23:08:25 -0500

    Update ItemStackClientMixin.java

--- src/main/java/elocindev/tierify/mixin/client/ItemStackClientMixin.java
+++ src/main/java/elocindev/tierify/mixin/client/ItemStackClientMixin.java
@@ -5,7 +5,6 @@ import com.google.common.collect.Multimap;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import draylar.tiered.api.PotentialAttribute;
-import elocindev.tierify.screen.client.PerfectMarkerComponent;
 import elocindev.tierify.screen.client.PerfectLabelAnimator;
 import elocindev.tierify.screen.client.TierGradientAnimator;
 import elocindev.tierify.Tierify;
