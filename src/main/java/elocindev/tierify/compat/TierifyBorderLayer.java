33450bb05666f05b4c24dd1375a070557a2b9624
Supergod99 <rsupergod99@gmail.com>
2025-12-06 19:14:41 -0500

    Create TierifyBorderLayer.java

---
+++ src/main/java/elocindev/tierify/compat/TierifyBorderLayer.java
@@ -0,0 +1,86 @@
+package elocindev.tierify.compat;
+
+import dev.xylonity.tooltipoverhaul.client.TooltipContext;
+import dev.xylonity.tooltipoverhaul.client.frame.CustomFrameData;
+import dev.xylonity.tooltipoverhaul.client.layer.ITooltipLayer;
+import dev.xylonity.tooltipoverhaul.client.layer.LayerDepth;
+import dev.xylonity.tooltipoverhaul.client.style.TooltipStyle;
+import elocindev.tierify.Tierify;
+import elocindev.tierify.TierifyClient;
+import draylar.tiered.api.BorderTemplate;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.class_241;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import java.awt.Point;
+
+public class TierifyBorderLayer implements ITooltipLayer {
+
+    @Override
+    public void render(TooltipContext ctx, class_241 pos, Point size, TooltipStyle style, Text rarity, TextRenderer font, CustomFrameData customFrame) {
+
+        if (!ctx.stack().hasNbt() || !ctx.stack().getSubNbt(Tierify.NBT_SUBTAG_KEY).contains(Tierify.NBT_SUBTAG_DATA_KEY)) {
+            return;
+        }
+
+
+        String tierId = ctx.stack().getSubNbt(Tierify.NBT_SUBTAG_KEY).getString(Tierify.NBT_SUBTAG_DATA_KEY);
+        boolean isPerfect = ctx.stack().getSubNbt(Tierify.NBT_SUBTAG_KEY).getBoolean("Perfect");
+        String lookupKey = isPerfect ? "{BorderTier:\"tiered:perfect\"}" : "{Tier:\"" + tierId + "\"}";
+
+
+        BorderTemplate match = null;
+        if (TierifyClient.BORDER_TEMPLATES != null) {
+            for (BorderTemplate template : TierifyClient.BORDER_TEMPLATES) {
+                if (template.containsDecider(lookupKey)) {
+                    match = template;
+                    break;
+                }
+            }
+        }
+
+        if (match == null) return;
+
+
+        int x = (int) pos.field_1343; 
+        int y = (int) pos.field_1342; 
+        int width = size.x;
+        int height = size.y;
+
+        int index = match.getIndex();
+        int secondHalf = index > 7 ? 1 : 0;
+        if (index > 7) index -= 8;
+        
+        Identifier texture = match.getIdentifier();
+
+
+        ctx.push(() -> {
+
+            ctx.translate(0.0f, 0.0f, LayerDepth.BACKGROUND_OVERLAY.getZ());
+            
+            DrawContext drawContext = ctx.graphics();
+            int texW = 128;
+            int texH = 128;
+
+            
+
+            drawContext.drawTexture(texture, x - 6, y - 6, 0 + secondHalf * 64, 0 + index * 16, 8, 8, texW, texH);
+            drawContext.drawTexture(texture, x + width - 2, y - 6, 56 + secondHalf * 64, 0 + index * 16, 8, 8, texW, texH);
+            drawContext.drawTexture(texture, x - 6, y + height - 2, 0 + secondHalf * 64, 8 + index * 16, 8, 8, texW, texH);
+            drawContext.drawTexture(texture, x + width - 2, y + height - 2, 56 + secondHalf * 64, 8 + index * 16, 8, 8, texW, texH);
+
+            drawContext.drawTexture(texture, x + 2, y - 6, width - 4, 8, 8 + secondHalf * 64, 0 + index * 16, 48, 8, texW, texH);
+
+            drawContext.drawTexture(texture, x + 2, y + height - 2, width - 4, 8, 8 + secondHalf * 64, 8 + index * 16, 48, 8, texW, texH);
+
+            drawContext.drawTexture(texture, x - 6, y + 2, 8, height - 4, 0 + secondHalf * 64, 8 + index * 16, 8, 8, texW, texH);
+
+            drawContext.drawTexture(texture, x + width - 2, y + 2, 8, height - 4, 56 + secondHalf * 64, 8 + index * 16, 8, 8, texW, texH);
+
+            if (width > 48) {
+                 drawContext.drawTexture(texture, x + (width / 2) - 24, y - 9, 8 + secondHalf * 64, 0 + index * 16, 48, 8, texW, texH);
+            }
+        });
+    }
+}
