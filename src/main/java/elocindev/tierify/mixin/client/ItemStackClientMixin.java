bcc1d749bf98292bb355b76b331b785330c043b5
Supergod99 <rsupergod99@gmail.com>
2025-12-11 00:44:26 -0500

    Update ItemStackClientMixin.java

--- src/main/java/elocindev/tierify/mixin/client/ItemStackClientMixin.java
+++ src/main/java/elocindev/tierify/mixin/client/ItemStackClientMixin.java
@@ -3,15 +3,12 @@ package elocindev.tierify.mixin.client;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.Multimap;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-
 import draylar.tiered.api.PotentialAttribute;
-import elocindev.tierify.screen.client.PerfectLabelAnimator;
-import elocindev.tierify.screen.client.TierGradientAnimator;
-import elocindev.tierify.util.SetBonusUtils;
 import elocindev.tierify.Tierify;
-import elocindev.tierify.util.TieredTooltip;
+import elocindev.tierify.screen.client.TierGradientAnimator;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttribute;
@@ -24,10 +21,6 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.text.OrderedText;
-
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -37,37 +30,20 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @SuppressWarnings({ "rawtypes", "unchecked" })
 @Environment(EnvType.CLIENT)
 @Mixin(ItemStack.class)
 public abstract class ItemStackClientMixin {
 
-    @Shadow
-    public abstract NbtCompound getOrCreateSubNbt(String key);
-
-    @Shadow
-    public abstract boolean hasNbt();
-
-    @Shadow
-    public abstract NbtCompound getSubNbt(String key);
-
-    @Shadow
-    @Final
-    @Mutable
-    public static DecimalFormat MODIFIER_FORMAT;
-    
-    // Shadow the method so we can call it manually
-    @Shadow
-    public abstract Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot);
+    @Shadow public abstract NbtCompound getOrCreateSubNbt(String key);
+    @Shadow public abstract boolean hasNbt();
+    @Shadow public abstract NbtCompound getSubNbt(String key);
+    @Shadow public abstract Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot);
+    @Shadow @Final @Mutable public static DecimalFormat MODIFIER_FORMAT;
 
     private boolean isTiered = false;
     private String translationKey;
@@ -83,19 +59,15 @@ public abstract class ItemStackClientMixin {
 
     @Inject(method = "getTooltip", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 6))
     private void storeTooltipInformation(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List> info) {
-
         for (EquipmentSlot slot : EquipmentSlot.values()) {
-            Multimap<EntityAttribute, EntityAttributeModifier> multimap = this.getAttributeModifiers(slot);
-            
-            for (Map.Entry<EntityAttribute, EntityAttributeModifier> entry : multimap.entries()) {
-                String translationKey = entry.getKey().getTranslationKey();
-                
-                if (entry.getValue().getName().contains("tiered:") && !map.containsKey(translationKey)) {
+            Multimap<EntityAttribute, EntityAttributeModifier> modifiers = this.getAttributeModifiers(slot);
+            for (Map.Entry<EntityAttribute, EntityAttributeModifier> entry : modifiers.entries()) {
+                if (entry.getValue().getName().contains("tiered:") && !map.containsKey(entry.getKey().getTranslationKey())) {
                     
                     double value = entry.getValue().getValue();
                     boolean isSetBonus = false;
                     PlayerEntity clientPlayer = MinecraftClient.getInstance().player;
-                    
+
                     if (value > 0 && clientPlayer != null && this.hasNbt()) {
                         NbtCompound nbt = this.getSubNbt(Tierify.NBT_SUBTAG_KEY);
                         if (nbt != null) {
@@ -104,14 +76,11 @@ public abstract class ItemStackClientMixin {
                                 int matchCount = 0;
                                 for (ItemStack armor : clientPlayer.getInventory().armor) {
                                     NbtCompound aNbt = armor.getSubNbt(Tierify.NBT_SUBTAG_KEY);
-                                    if (aNbt != null && aNbt.getString(Tierify.NBT_SUBTAG_DATA_KEY).equals(myTier)) {
-                                        matchCount++;
-                                    }
+                                    if (aNbt != null && aNbt.getString(Tierify.NBT_SUBTAG_DATA_KEY).equals(myTier)) matchCount++;
                                 }
-                                
                                 if (matchCount >= 4) {
-                                    value *= 1.25D;  
-                                    isSetBonus = true; 
+                                    value *= 1.25D;
+                                    isSetBonus = true;
                                 }
                             }
                         }
@@ -123,14 +92,23 @@ public abstract class ItemStackClientMixin {
                                     : (entry.getKey().equals(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE) ? value * 10.0 : value));
 
                     ArrayList collect = new ArrayList<>();
-                    collect.add(entry.getValue().getOperation().getId()); 
-                    collect.add(format);                                  
-                    collect.add(value > 0.0D);                            
-                    collect.add(isSetBonus); 
-                    map.put(translationKey, collect);
+                    collect.add(entry.getValue().getOperation().getId());
+                    collect.add(format);
+                    collect.add(value > 0.0D);
+                    collect.add(isSetBonus);
+                    map.put(entry.getKey().getTranslationKey(), collect);
                 }
             }
         }
+    }
+
+    @Redirect(method = "getTooltip", at = @At(value = "INVOKE", target = "Ljava/util/Map$Entry;getKey()Ljava/lang/Object;", ordinal = 0))
+    private Object captureAttributeKey(Map.Entry entry) {
+        Object key = entry.getKey();
+        if (key instanceof EntityAttribute attr) {
+            this.translationKey = attr.getTranslationKey();
+        }
+        return key;
     }
 
     @Redirect(method = "getTooltip", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 8))
@@ -154,6 +132,8 @@ public abstract class ItemStackClientMixin {
     @Redirect(method = "getTooltip", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 9))
     private boolean modifyTooltipTake(List<Text> list, Object text) {
         if (this.map != null && !this.map.isEmpty() && this.map.containsKey(this.translationKey)) {
+            
+             list.add((Text) text);
         } else {
             list.add((Text) text);
         }
@@ -164,7 +144,6 @@ public abstract class ItemStackClientMixin {
     private boolean modifyTooltipEquals(List<Text> list, Object text) {
         if (this.map != null && !this.map.isEmpty() && this.map.containsKey(this.translationKey)) {
             ArrayList collected = map.get(translationKey);
-            
             boolean isSetBonus = collected.size() > 3 && (boolean) collected.get(3);
             Formatting format = isSetBonus ? Formatting.GOLD : Formatting.DARK_GREEN;
 
@@ -172,17 +151,10 @@ public abstract class ItemStackClientMixin {
                     (isSetBonus ? "§6§l" : "§2") + " " + this.armorModifierFormat,
                     ((boolean) collected.get(2) ? (isSetBonus ? "§6§l" : "§2") + "(+" : "§c(") + (String) collected.get(1) + ((int) collected.get(0) > 0 ? "%)" : ")"),
                     Text.translatable(translationKey).formatted(format)));
-            
         } else {
             list.add((Text) text);
         }
         return true;
-    }
-
-
-    @Inject(method = "getTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/EntityAttributeModifier;getOperation()Lnet/minecraft/entity/attribute/EntityAttributeModifier$Operation;", ordinal = 0))
-    private void storeAttributeModifier(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List> cir) {
-
     }
 
     @Redirect(method = "getTooltip",
@@ -190,9 +162,6 @@ public abstract class ItemStackClientMixin {
         target = "Lnet/minecraft/text/MutableText;formatted(Lnet/minecraft/util/Formatting;)Lnet/minecraft/text/MutableText;",
         ordinal = 2))
     private MutableText getFormatting(MutableText text, Formatting formatting) {
-    
-        System.out.println("TIERIFY DEBUG: getFormatting called for text: " + text.getString());
-        
         String raw = text.getString();
         if (raw.contains("§x")) {
             return text;
@@ -226,29 +195,21 @@ public abstract class ItemStackClientMixin {
 
     @Inject(method = "getName", at = @At("RETURN"), cancellable = true)
     private void getNameMixin(CallbackInfoReturnable<Text> info) {
-
         if (this.hasNbt() && this.getSubNbt("display") == null && this.getSubNbt(Tierify.NBT_SUBTAG_KEY) != null) {
-    
-            Identifier tier = new Identifier(getOrCreateSubNbt(Tierify.NBT_SUBTAG_KEY)
-                    .getString(Tierify.NBT_SUBTAG_DATA_KEY));
-    
-            PotentialAttribute potentialAttribute =
-                    Tierify.ATTRIBUTE_DATA_LOADER.getItemAttributes().get(tier);
-    
+            Identifier tier = new Identifier(getOrCreateSubNbt(Tierify.NBT_SUBTAG_KEY).getString(Tierify.NBT_SUBTAG_DATA_KEY));
+            PotentialAttribute potentialAttribute = Tierify.ATTRIBUTE_DATA_LOADER.getItemAttributes().get(tier);
             if (potentialAttribute != null) {
                 MutableText text = Text.translatable(potentialAttribute.getID() + ".label");
-                String tierKey = elocindev.tierify.screen.client.TierGradientAnimator.getTierFromId(potentialAttribute.getID());
-                text = elocindev.tierify.screen.client.TierGradientAnimator.animate(text, tierKey);
+                String tierKey = TierGradientAnimator.getTierFromId(potentialAttribute.getID());
+                text = TierGradientAnimator.animate(text, tierKey);
                 MutableText vanilla = info.getReturnValue().copy();
                 info.setReturnValue(text.append(" ").append(vanilla));
             }
         }
     }
 
-
     @Inject(method = "getTooltip", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemStack;getAttributeModifiers(Lnet/minecraft/entity/EquipmentSlot;)Lcom/google/common/collect/Multimap;"))
     private void getTooltipMixin(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> info) {
-
         if (this.hasNbt()) {
             NbtCompound tierTag = this.getSubNbt(Tierify.NBT_SUBTAG_KEY);
             if (tierTag != null && tierTag.getBoolean("Perfect")) {
