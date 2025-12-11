fc014622a35db0c5e6f0a3e020e1725e04e01c6b
Supergod99 <rsupergod99@gmail.com>
2025-12-11 16:27:14 -0500

    Create DrawContentMixin.java

---
+++ src/main/java/elocindev/tierify/mixin/client/DrawContentMixin.java
@@ -0,0 +1,64 @@
+package elocindev.tierify.mixin.client;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import elocindev.tierify.Tierify;
+import elocindev.tierify.TierifyClient;
+import elocindev.tierify.util.TieredTooltip;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.tooltip.HoveredTooltipPositioner;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+
+@Environment(EnvType.CLIENT)
+@Mixin(DrawContext.class)
+public class DrawContextMixin {
+
+    @Inject(method = "drawItemTooltip", at = @At("HEAD"), cancellable = true)
+    private void drawItemTooltipMixin(TextRenderer textRenderer, ItemStack stack, int x, int y, CallbackInfo info) {
+        if (FabricLoader.getInstance().isModLoaded("tooltipoverhaul") || FabricLoader.getInstance().isModLoaded("legendarytooltips")) {
+            return;
+        }
+
+        if (Tierify.CLIENT_CONFIG.tieredTooltip && stack.hasNbt() && stack.getOrCreateSubNbt(Tierify.NBT_SUBTAG_KEY) != null) {
+
+            String tier = stack.getOrCreateSubNbt(Tierify.NBT_SUBTAG_KEY).getString(Tierify.NBT_SUBTAG_DATA_KEY);
+            String lookupKey = "{Tier:\"" + tier + "\"}";
+
+
+            for (int i = 0; i < TierifyClient.BORDER_TEMPLATES.size(); i++) {
+                if (TierifyClient.BORDER_TEMPLATES.get(i).containsDecider(lookupKey)) {
+
+                    List<Text> text = net.minecraft.client.gui.screen.Screen.getTooltipFromItem(MinecraftClient.getInstance(), stack);
+                    List<TooltipComponent> list = text.stream().map(Text::asOrderedText).map(TooltipComponent::of).collect(Collectors.toList());
+                    stack.getTooltipData().ifPresent(data -> list.add(1, TooltipComponent.of(data)));
+
+                    TieredTooltip.renderTieredTooltipFromComponents(
+                        (DrawContext) (Object) this, 
+                        textRenderer, 
+                        list, 
+                        x, 
+                        y, 
+                        HoveredTooltipPositioner.INSTANCE, 
+                        TierifyClient.BORDER_TEMPLATES.get(i)
+                    );
+                  
+                    info.cancel();
+                    break;
+                }
+            }
+        }
+    }
+}
