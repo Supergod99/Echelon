df3dd04e55a4d60278ea0379ecb42ba3fb0b72ae
Supergod99 <rsupergod99@gmail.com>
2025-12-10 22:57:34 -0500

    Update TieredDrawContextMixin.java

--- src/main/java/elocindev/tierify/mixin/client/TieredDrawContextMixin.java
+++ src/main/java/elocindev/tierify/mixin/client/TieredDrawContextMixin.java
@@ -1,61 +1,66 @@
 package elocindev.tierify.mixin.client;
 
-import draylar.tiered.api.BorderTemplate;
-import elocindev.tierify.Tierify;
-import elocindev.tierify.TierifyClient;
-import elocindev.tierify.util.TieredTooltip;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.tooltip.TooltipComponent;
-import net.minecraft.client.gui.tooltip.TooltipPositioner;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
+import elocindev.tierify.TierifyClient;
+import elocindev.tierify.Tierify;
+import elocindev.tierify.util.TieredTooltip;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.tooltip.HoveredTooltipPositioner;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 
 @Mixin(DrawContext.class)
 public class TieredDrawContextMixin {
 
-    @Inject(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V", at = @At("HEAD"), cancellable = true)
-    private void renderTieredTooltip(TextRenderer textRenderer, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner, CallbackInfo ci) {
-        System.out.println("TIERIFY DEBUG: TieredDrawContextMixin is running!");
+    @Shadow
+    @Mutable
+    @Final
+    private MinecraftClient client;
+
+    @Inject(method = "drawItemTooltip", at = @At("HEAD"), cancellable = true)
+    private void drawItemTooltipMixin(TextRenderer textRenderer, ItemStack stack, int x, int y, CallbackInfo info) {
+
         if (FabricLoader.getInstance().isModLoaded("tooltipoverhaul")) {
             return;
         }
 
-        if (!Tierify.CLIENT_CONFIG.tieredTooltip) {
-            return;
-        }
+        if (Tierify.CLIENT_CONFIG.tieredTooltip && stack.hasNbt() && stack.getNbt().contains(Tierify.NBT_SUBTAG_KEY)) {
+            String tierId = stack.getSubNbt(Tierify.NBT_SUBTAG_KEY).getString(Tierify.NBT_SUBTAG_DATA_KEY);
 
-        ItemStack stack = TierifyClient.CURRENT_TOOLTIP_STACK;
-        if (stack.isEmpty() || !stack.hasNbt()) {
-            return;
-        }
-
-        NbtCompound nbt = stack.getSubNbt(Tierify.NBT_SUBTAG_KEY);
-        System.out.println("TIERIFY DEBUG: Item " + stack.getName().getString() + " has Tier NBT? " + (nbt != null));
-        if (nbt == null) {
-            return;
-        }
-        
-        String tierId = nbt.getString(Tierify.NBT_SUBTAG_DATA_KEY);
-        boolean isPerfect = nbt.getBoolean("Perfect");
-        String lookupKey = isPerfect ? "{BorderTier:\"tiered:perfect\"}" : "{Tier:\"" + tierId + "\"}";
-        BorderTemplate borderTemplate = null;
-        for (BorderTemplate template : TierifyClient.BORDER_TEMPLATES) {
-            if (template.containsDecider(lookupKey)) {
-                borderTemplate = template;
-                break;
+            String lookupKey = "{Tier:\"" + tierId + "\"}";
+            
+            if (stack.getSubNbt(Tierify.NBT_SUBTAG_KEY).getBoolean("Perfect")) {
+                lookupKey = "{BorderTier:\"tiered:perfect\"}";
             }
-        }
-        if (borderTemplate != null) {
-            TieredTooltip.renderTieredTooltipFromComponents((DrawContext) (Object) this, textRenderer, components, x, y, positioner, borderTemplate);
-            ci.cancel(); 
+
+            for (int i = 0; i < TierifyClient.BORDER_TEMPLATES.size(); i++) {
+                if (TierifyClient.BORDER_TEMPLATES.get(i).containsDecider(lookupKey)) {
+
+                    List<Text> text = Screen.getTooltipFromItem(client, stack);
+                    List<TooltipComponent> list = text.stream().map(Text::asOrderedText).map(TooltipComponent::of).collect(Collectors.toList());
+                    stack.getTooltipData().ifPresent(data -> list.add(1, TooltipComponent.of(data)));
+
+                    TieredTooltip.renderTieredTooltipFromComponents((DrawContext) (Object) this, textRenderer, list, x, y, HoveredTooltipPositioner.INSTANCE, TierifyClient.BORDER_TEMPLATES.get(i));
+                    
+                    info.cancel();
+                    break;
+                }
+            }
         }
     }
 }
